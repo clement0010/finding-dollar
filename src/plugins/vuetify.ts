99087467a83c9c8b998827fbf3319d06bcbc5e64
Clement Tee <clement@usefriz.com>
2021-07-28 16:48:36 +0800

    feat: plugin setup

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,38 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import { VuetifyParsedTheme, VuetifyThemeCache } from 'vuetify/types/services/theme';
+
+import minifyTheme from 'minify-css-string';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+const opts = {
+  theme: {
+    themes: {
+      light: {
+        anchor: '#0E49A6',
+        primary: '#F3F3FF',
+        secondary: '#0E49A6',
+        accent1: '#07BF9B',
+        accent2: '#C7C7E4',
+        accent3: '#979797',
+        white: '#FFFFFF',
+        black: '#000000',
+        success: '#07BF9B',
+        warning: '#FBC046',
+        error: '#E10050',
+      },
+    },
+    options: {
+      themeCache: {
+        get: (key: VuetifyParsedTheme) => localStorage.getItem(key as unknown as string),
+        set: (key: VuetifyParsedTheme, value: string) =>
+          localStorage.setItem(key as unknown as string, value),
+      } as VuetifyThemeCache,
+      variations: false,
+      minifyTheme,
+    },
+  },
+};
+
+export default new Vuetify(opts);
