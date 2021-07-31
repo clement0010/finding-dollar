9aeab3336ce7a8d4876c6e7434f5fb163f5f804c
Clement Tee <clement@usefriz.com>
2021-08-01 06:40:47 +0800

    feat: add views

---
+++ src/views/Error.vue
@@ -0,0 +1,20 @@
+<template>
+  <BasePage>
+    <v-container fluid class="ma-0 pa-0 justify-center">
+      <v-row no-gutters justify="center"> This is error page </v-row>
+    </v-container>
+  </BasePage>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+import BasePage from '@/layouts/BasePage.vue';
+
+export default defineComponent({
+  name: 'Error',
+  components: {
+    BasePage,
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,55 @@
 <template>
   <BasePage>
-    <HomeLayout />
+    <v-card flat color="transparent">
+      <v-card-title
+        class="text-center secondary--text font-weight-bold justify-center"
+        :class="[$vuetify.breakpoint.mdAndUp ? 'display-4' : 'display-3']"
+      >
+        ICM$ SG:
+        <br />
+        Finding Dollar
+      </v-card-title>
+      <v-card-actions class="justify-center">
+        <v-btn class="rounded-lg secondary" elevation="5">14 AUG 2021</v-btn>
+        <v-btn class="rounded-lg secondary" elevation="5">9 AM - 3 PM (SGT)</v-btn>
+      </v-card-actions>
+    </v-card>
   </BasePage>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { computed, defineComponent } from '@vue/composition-api';
 
 import BasePage from '@/layouts/BasePage.vue';
-import HomeLayout from '@/layouts/HomeLayout.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     BasePage,
-    HomeLayout,
+  },
+  setup(_, { root }) {
+    const test = computed(() => {
+      switch (root.$vuetify.breakpoint.name) {
+        case 'xs':
+          console.log('I am XS');
+          return 200;
+        case 'sm':
+          console.log('I am SM');
+          return 300;
+        case 'md':
+          console.log('I am MD');
+          return 300;
+        case 'lg':
+          console.log('I am LG');
+          return 450;
+        case 'xl':
+          console.log('I am XL');
+          return 450;
+      }
+    });
+    return {
+      test,
+    };
   },
 });
 </script>
